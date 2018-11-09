f0afd5f64ab9ee70162b95c5c11b5aedd2560004
Mayara Castro <maaycastro@hotmail.com>
2018-11-09 13:58:24 -0200

    Tem que ajeitar o login. Escolhi esse estilo de activity pra a tela principal. to testando aqui.

--- app/src/main/java/com/example/sturing/sturing/LoginActivity.kt
+++ app/src/main/java/com/example/sturing/sturing/LoginActivity.kt
@@ -1,12 +1,19 @@
 package com.example.sturing.sturing
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btLogin.setOnClickListener {
+            val i = Intent(this,MainActivity::class.java)
+            startActivity(i)
+        }
     }
 }
