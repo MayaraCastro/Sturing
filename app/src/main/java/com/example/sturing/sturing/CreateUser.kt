85c0430c1e99d7832ec9f75ec6dae3690bed8861
Mayara Castro <maaycastro@hotmail.com>
2018-11-17 19:13:00 -0200

    tela de criar usuário

---
+++ app/src/main/java/com/example/sturing/sturing/CreateUser.kt
@@ -0,0 +1,19 @@
+package com.example.sturing.sturing
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_create_user.*
+
+class CreateUser : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+        setSupportActionBar(toolbar)
+
+
+    }
+
+}
