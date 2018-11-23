cbddb54b6f3414ec05d8bde3953a742c5e99c065
Wilder Santos Carvalho <wcarvalho98@gmail.com>
2018-11-22 22:09:20 -0300

    Queria chamar outra tela? Entao toma!

--- app/src/main/java/com/example/sturing/sturing/GroupAdapter.kt
+++ app/src/main/java/com/example/sturing/sturing/GroupAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.sturing.sturing
 
 import android.content.Context
+import android.content.Intent
 import android.support.design.widget.Snackbar
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -8,10 +9,12 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.group_list_item.view.*
 
-class GroupAdapter(var items : ArrayList<Group>, var context : Context) : RecyclerView.Adapter<ViewHolder>() {
+class GroupAdapter(var items : ArrayList<Group>, var context : Context) : RecyclerView.Adapter<GroupAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.group_list_item, p0, false))
+        var view = LayoutInflater.from(p0.context).inflate(R.layout.group_list_item, p0, false)
+        var holder = ViewHolder(view)
+        return holder
     }
 
     override fun getItemCount(): Int {
@@ -21,22 +24,23 @@ class GroupAdapter(var items : ArrayList<Group>, var context : Context) : Recycl
     override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
         p0.tvGroupName?.text = items[p1].getName()
         p0.tvGroupDescription?.text = items[p1].getDescription()
-        p0.tvGroupTimestamp?.text = items[p1].getTimestamp()
         //p0.ivGroupImage?.setImageResource(items[p1].getImage()!!.id)
         p0.itemView.setOnClickListener {
             Snackbar.make(it, items[p1].getName(), Snackbar.LENGTH_SHORT)
                     .setAction("Action", null).show()
+            val i = Intent(context, MainActivity::class.java)
+            context.startActivity(i)
         }
+    }
+
+    class ViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+        // Holds the TextView that will add each animal to
+        val tvGroupName = view.tvGroupName
+        val tvGroupDescription = view.tvGroupDescription
+        val ivGroupImage = view.ivGroupImage
+        val cltGroupItem = view.groupItem
+
     }
 
 }
 
-class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
-    // Holds the TextView that will add each animal to
-
-    val tvGroupName = view.tvGroupName
-    val tvGroupDescription = view.tvGroupDescription
-    val tvGroupTimestamp = view.tvGroupTimestamp
-    val ivGroupImage = view.ivGroupImage
-
-}
