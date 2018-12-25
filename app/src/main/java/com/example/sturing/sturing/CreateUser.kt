3c4dd65c08a9f50e2f0254f501a12d905ae91a24
Wilder Santos Carvalho <wcarvalho98@gmail.com>
2018-12-25 17:02:01 -0300

    Verificacao de erro na hora de registro de usuario tambem

--- app/src/main/java/com/example/sturing/sturing/CreateUser.kt
+++ app/src/main/java/com/example/sturing/sturing/CreateUser.kt
@@ -1,8 +1,14 @@
 package com.example.sturing.sturing
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.annotation.TargetApi
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
+import android.view.View
 import kotlinx.android.synthetic.main.activity_create_user.*
 import kotlinx.android.synthetic.main.content_create_user.*
 import android.widget.Toast
@@ -21,20 +27,102 @@ class CreateUser : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         btSignup.setOnClickListener {
-            email = txtEmail.text.toString()
-            password = txtPass.text.toString()
-            mAuth.createUserWithEmailAndPassword(email!!, password!!)
+            signup()
+        }
+
+        btSignin.setOnClickListener {onBackPressed()}
+    }
+
+    private fun signup() {
+        val name = txtName.text.toString()
+        val username = txtUsername.text.toString()
+
+        txtName.error = null
+        txtPass.error = null
+        txtUsername.error = null
+        txtEmail.error = null
+
+        email = txtEmail.text.toString()
+        password = txtPass.text.toString()
+
+        var cancel = false
+        var focusView: View? = null
+
+        if (TextUtils.isEmpty(password) || !isPasswordValid(password)) {
+            txtPass.error = getString(R.string.error_invalid_password)
+            focusView = txtPass
+            cancel = true
+        }
+
+        if (TextUtils.isEmpty(username)) {
+            txtUsername.error = getString(R.string.error_field_required)
+            focusView = txtUsername
+            cancel = true
+        }
+
+        if (TextUtils.isEmpty(email)) {
+            txtEmail.error = getString(R.string.error_field_required)
+            focusView = txtEmail
+            cancel = true
+        } else if (!isEmailValid(email)) {
+            txtEmail.error = getString(R.string.error_invalid_email)
+            focusView = txtEmail
+            cancel = true
+        }
+
+        if (TextUtils.isEmpty(name)) {
+            txtName.error = getString(R.string.error_field_required)
+            focusView = txtName
+            cancel = true
+        }
+
+        if (cancel) {
+            focusView?.requestFocus()
+        } else {
+            showProgress(true)
+            mAuth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this) { task ->
+                        showProgress(false)
                         if (task.isSuccessful) {
                             val i = Intent(this,LoginActivity::class.java)
                             startActivity(i)
                         } else {
-                            Toast.makeText(this@CreateUser, "Authentication failed.",
+                            Toast.makeText(this@CreateUser, getString(R.string.registration_failed),
                                     Toast.LENGTH_SHORT).show()
                         }
                     }
         }
+    }
 
-        btSignin.setOnClickListener {onBackPressed()}
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private fun showProgress(show: Boolean) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            val shortAnimTime = resources.getInteger(android.R.integer.config_shortAnimTime).toLong()
+
+            register_progress.visibility = if (show) View.VISIBLE else View.GONE
+            register_progress.animate()
+                    .setDuration(shortAnimTime)
+                    .alpha((if (show) 1 else 0).toFloat())
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator) {
+                            register_progress.visibility = if (show) View.VISIBLE else View.GONE
+                        }
+                    })
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            register_progress.visibility = if (show) View.VISIBLE else View.GONE
+        }
+    }
+
+    private fun isEmailValid(email: String): Boolean {
+        return email.contains("@")
+    }
+
+    private fun isPasswordValid(password: String): Boolean {
+        return password.length > 4
     }
 }
